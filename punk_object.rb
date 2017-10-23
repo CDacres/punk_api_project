cd4152c0ee0c29890562a3ea81b73eadc2a2b517
Christian Dacres <christian_dacres@hotmail.co.uk>
2017-10-23 15:00:14 +0100

    fixed single beer request

--- punk_object.rb
+++ punk_object.rb
@@ -6,12 +6,16 @@ class BeersService
 
   base_uri 'https://api.punkapi.com/v2'
 
-  def get_single_beer(beer_num)
-    JSON.parse(self.class.get("/beers/#{beer_num}").body)
+  def get_single_beer(beer_id)
+    JSON.parse(self.class.get("/beers/#{beer_id}").body)
   end
 
   def get_random_beer
     JSON.parse(self.class.get("/beers/random").body)
   end
 
+
 end
+
+beer = BeersService.new
+p beer.get_single_beer("1")
