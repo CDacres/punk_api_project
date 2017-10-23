3c458f342b3cba5d5a5e5b06c4ee2ff9655ea6a7
Christian Dacres <christian_dacres@hotmail.co.uk>
2017-10-23 16:39:12 +0100

    completed search methods for beer

--- punk_object.rb
+++ punk_object.rb
@@ -4,6 +4,12 @@ require 'json'
 class BeersService
   include HTTParty
 
+  def initialize
+    @key_array = []
+    @value_array = []
+    @url_params = ""
+  end
+
   SEARCH_PARAMS = %w(abv_gt abv_lt ibu_gt ibu_lt ebc_gt ebc_lt beer_name yeast brewed_before brewed_after hops malt food ids)
 
   base_uri 'https://api.punkapi.com/v2'
@@ -19,15 +25,35 @@ class BeersService
   def check_params_valid(params_hash)
     params_hash.each do |key, value|
       if SEARCH_PARAMS.include?(key)
-        p value
+        @key_array << key
+        @value_array << value
       else
         raise "No valid parameters entered"
       end
     end
   end
 
+  def add_params_to_url
+    if (@key_array.length > 0 && @value_array.length > 0)
+      if (@key_array.length == 1 && @value_array.length == 1)
+        @url_params << "?#{@key_array[0]}=#{@value_array[0]}"
+      else
+        @url_params << "?#{@key_array[0]}=#{@value_array[0]}"
+        for i in 1..(@key_array.length)-1 do
+          @url_params << "&#{@key_array[i]}=#{@value_array[i]}"
+        end
+      end
+    end
+  end
+
+  def get_beer_search_results
+    JSON.parse(self.class.get("/beers#{@url_params}").body)
+  end
+
 end
 
 beer = BeersService.new
 # p beer.get_single_beer("1")
-beer.check_params_valid({'abv_gt' => 5})
+beer.check_params_valid({'abv_gt' => '8', 'yeast' => 'Wyeast_1056_-_American_Ale'})
+beer.add_params_to_url
+p beer.get_beer_search_results
