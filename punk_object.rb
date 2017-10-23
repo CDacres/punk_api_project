371a736d2708ab1a0f1075a4245249483be88d72
Christian Dacres <christian_dacres@hotmail.co.uk>
2017-10-23 15:47:07 +0100

    added validation function for search params

--- punk_object.rb
+++ punk_object.rb
@@ -4,6 +4,8 @@ require 'json'
 class BeersService
   include HTTParty
 
+  SEARCH_PARAMS = %w(abv_gt abv_lt ibu_gt ibu_lt ebc_gt ebc_lt beer_name yeast brewed_before brewed_after hops malt food ids)
+
   base_uri 'https://api.punkapi.com/v2'
 
   def get_single_beer(beer_id)
@@ -14,8 +16,18 @@ class BeersService
     JSON.parse(self.class.get("/beers/random").body)
   end
 
+  def check_params_valid(params_hash)
+    params_hash.each do |key, value|
+      if SEARCH_PARAMS.include?(key)
+        p value
+      else
+        raise "No valid parameters entered"
+      end
+    end
+  end
 
 end
 
 beer = BeersService.new
-p beer.get_single_beer("1")
+# p beer.get_single_beer("1")
+beer.check_params_valid({'abv_gt' => 5})
